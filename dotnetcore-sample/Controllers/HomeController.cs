09b47fde8e1709d42cf0b4ebcb4660dd424337a1
Joe Shepherd <Joeshepherdmba@outlook.com>
2018-02-28 13:18:31 -0600

    testing CD

--- dotnetcore-sample/Controllers/HomeController.cs
+++ dotnetcore-sample/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace dotnetcore_sample.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Testing CI/CD.";
 
             return View();
         }
